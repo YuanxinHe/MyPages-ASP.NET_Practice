60d71b8ca6e3c135ebf2ecaaecc4d7bc9a81790f
YuanxinHe <yuanxinhe89@gmail.com>
2016-11-11 14:40:29 -0500

    Applying RESTful service for movies

---
+++ MyPages/DTOs/MovieDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using MyPages.Models;
+
+namespace MyPages.DTOs
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        public byte GenreId { get; set; }
+
+        public DateTime ReleaseTime { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [Range(1, 20)]
+        public byte NumberInStock { get; set; }
+    }
+}
